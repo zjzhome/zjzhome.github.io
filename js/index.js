85af71bcb15279945fe71f3bd36a09da7a0b81e2
zjzhome <zjzhome@126.com>
2017-03-01 21:40:39 +0800

    增加侧滑

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 
 (function() {
 
+    var Page = {
+
+        initEvent: function() {
+            var avatar = $('#avatar');
+            var pushes = $('.j-push');
+            var sidebar = $('#sidebar');
+            var clicked = false;
+
+            avatar.click(function() {
+                if(clicked) {
+                    sidebar.removeClass('pushed');
+                    pushes.removeClass('pushed');
+                } else {
+                    sidebar.addClass('pushed');
+                    pushes.addClass('pushed');
+                }
+
+                clicked = !clicked;
+            });
+        },
+
+        init: function() {
+            this.initEvent();
+        }
+    }
+
+    Page.init();
 
 })();
