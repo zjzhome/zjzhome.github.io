a1c1dd258b65742e056deb35017ba855f0cb45a3
zjzhome <zjzhome@126.com>
2017-03-03 23:44:15 +0800

    add about

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@
     var Page = {
 
         initEvent: function() {
+            this.toggleSidebar();
+            this.toggleAbout();
+        },
+
+        toggleSidebar: function() {
             var avatar = $('#avatar');
             var pushes = $('.j-push');
             var sidebar = $('#sidebar');
@@ -21,6 +26,40 @@
                 }
 
                 clicked = !clicked;
+            });
+        },
+
+        toggleAbout: function() {
+            var toabout = $('#toabout');
+            var blog = $('#blog');
+            var about = $('#about');
+            var card = about.find('.about-card');
+            var close = about.find('.about-card-close');
+
+            toabout.click(function(e) {
+                e.preventDefault();
+                blog.css('opacity', 0);
+                setTimeout(function() {
+                    blog.css('display', 'none');
+                    about.css('display', 'block');
+
+                    setTimeout(function() {
+                        card.css('-webkit-transform', 'translate3d(-50%, -50%, 0)');
+                        card.css('transform', 'translate3d(-50%, -50%, 0)');
+                    }, 250);
+                }, 300);
+            });
+
+            close.click(function() {
+                card.css('-webkit-transform', 'translate3d(-50%, -1000px, 0)');
+                card.css('transform', 'translate3d(-50%, -1000px, 0)');
+                setTimeout(function() {
+                    blog.css('display', 'block');
+                    setTimeout(function() {
+                        about.css('display', 'none');
+                        blog.css('opacity', 1);
+                    }, 300);
+                }, 450);
             });
         },
 
